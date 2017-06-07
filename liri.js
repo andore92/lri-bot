0e14f91769df0b742e220ea0365c7dbbbfe23be1
Andrew Sadowski <Andrew Sadowski>
2017-06-07 06:49:09 -0400

    omdb api functionality working, can search for movies, if no movie is input it automaticaly prints out information for Mr Nobody

--- liri.js
+++ liri.js
@@ -1,6 +1,8 @@
 var twitter = require("twitter");
 var spotifyWebApi = require('spotify-web-api-node');
 var keys = require("./keys.js");
+var request = require("request");
+
 var twitterKeys = keys.twitterKeys
 var spotifyKeys = keys.spotifyKeys
 
@@ -19,6 +21,10 @@ userSpotify.setAccessToken('BQB_3kJANp3C7NtSIonEXh8N1Mcg9ohVdA9smbEPwOsQGm2UCoKi
 
 var apiArg = process.argv[2];
 var mediaArg = JSON.stringify(process.argv[3]);
+
+var queryUrl = "http://www.omdbapi.com/?t=" + mediaArg + "&y=&plot=short&apikey=ca876342";
+
+
 
 var twitterParams = {screen_name: 'fakegeekguy', count: '20'};
 
@@ -52,4 +58,39 @@ if (apiArg === "my-tweets") {
     console.log("Song name: The Sign");
     console.log("Preview URL: https://p.scdn.co/mp3-preview/4c463359f67dd3546db7294d236dd0ae991882ff?cid=8897482848704f2a8f8d7c79726a70d4");
   });
-} 
+} else if (apiArg === "movies-this") {
+	if (mediaArg != null) {
+		request(queryUrl, function(error, response, body) {
+
+	  
+	  if (!error && response.statusCode === 200) {
+
+	    
+	    console.log("Title: " + JSON.parse(body).Title);
+	    console.log("Release Year: " + JSON.parse(body).Year);
+	    console.log("IMDB Rating: " + JSON.parse(body).Ratings[0].Value);
+	    console.log("Country: " + JSON.parse(body).Country);
+	    console.log("Language: " + JSON.parse(body).Language);
+	    console.log("Plot: " + JSON.parse(body).Plot);
+	    console.log("Actors: " + JSON.parse(body).Actors);
+	    console.log("Website: " + JSON.parse(body).Website);
+
+	  } else {
+	  	console.log(error);
+	  }
+	 });
+	}	else{
+	  	
+	  	console.log("Title: Mr. Nobody");
+	    console.log("Release Year: 2013");
+	    console.log("IMDB Rating: 7.9/10");
+	    console.log("Country: Belgium, Germany, Canada, France, USA, UK");
+	    console.log("Language: English, Mohawk");
+	    console.log("Plot: A boy stands on a station platform as a train is about to leave. Should he go with his mother or stay with his father? Infinite possibilities arise from this decision. As long as he doesn't choose, anything is possible");
+	    console.log("Actors: Jared Leto, Sarah Polley, Diane Kruger, Linh Dan Pham");
+	    console.log("Website: http://www.magpictures.com/mrnobody/");
+	  }
+	
+} else if (apiArg === "do-what-it-says") {
+	
+}
